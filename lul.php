64f7b0900a6dc7c283489f8b9eb913a863616adb
Michael Fairchild <mfairchild365@gmail.com>
2015-07-30 18:28:02 -0500

    Add support for twitter lists

--- lul.php
+++ lul.php
@@ -4,16 +4,26 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 require_once __DIR__ . '/config.inc.php';
 
-$url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
+if (!isset($argv[1])) {
+  echo 'usage: lul.php username [list_name]' . PHP_EOL;
+  exit();
+}
 
-$argument1 = $argv[1];
+$url            = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
+$user_name      = $argv[1];
+$query_string   = '?screen_name='.$user_name;
+$request_method = 'GET';
 
-$getfield = '?screen_name='.$argument1;
-$requestMethod = 'GET';
+$list = '';
+if (isset($argv[2])) {
+  $list = $argv[2];
+  $url = 'https://api.twitter.com/1.1/lists/statuses.json';
+  $query_string = '?owner_screen_name='.$user_name.'&slug='.$list;
+}
 
 $twitter = new TwitterAPIExchange($settings);
-$result = $twitter->setGetfield($getfield)
-    ->buildOauth($url, $requestMethod)
+$result = $twitter->setGetfield($query_string)
+    ->buildOauth($url, $request_method)
     ->performRequest();
 
 $result = json_decode($result);
@@ -39,15 +49,15 @@ if (!is_array($result)) {
     exit();
 }
 
-$newest = array_shift($result)->text;
+$newest = array_shift($result);
 
-$storage_dir = __DIR__ . '/tweets/';
+$storage_dir = __DIR__ . '/tweets';
 
 if (!file_exists($storage_dir)) {
     mkdir($storage_dir, 0777, true);
 }
 
-$storage_file = $storage_dir.$argument1.'.txt';
+$storage_file = $storage_dir.'/'.$user_name.'_'.$list.'.txt';
 
 if (!file_exists($storage_file)) {
   file_put_contents($storage_file, '');
@@ -57,5 +67,5 @@ $last = file_get_contents($storage_file);
 
 if ($newest != $last) {
   file_put_contents($storage_file, $newest);
-  echo $newest . PHP_EOL;
+  echo $newest->user->name . ' says ' . $newest->text . PHP_EOL;
 }
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 while true
 do
-  php ./lul.php $1 | say
+  php ./lul.php $1 $2 | say
   sleep 30
 done
 
